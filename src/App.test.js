ef24cd2e5c1d1ab70c9a6ad30fc894945592f5e5
Ibrahim Saifee <ibrahim.saifee@infobeans.com>
2021-09-03 19:36:54 +0530

    Fixed the test case for App.jsx

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import store from "./app/store";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders learn react link", () => {
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+  const linkElement = screen.getByText(/10up Blog/i);
   expect(linkElement).toBeInTheDocument();
 });
