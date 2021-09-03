c2e87654617eed6ffbd74624f28e0991986a212e
Ibrahim Saifee <ibrahim.saifee@infobeans.com>
2021-09-03 11:04:09 +0530

    Added form for login with username and password. Store the session informatio in redux state and display username from session on home page.

--- src/features/home/index.jsx
+++ src/features/home/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./index.css";
 import Posts from "../posts/index";
+import { selectSession } from "../../app/sessionSlice";
 
 const Home = () => {
+  const session = useSelector(selectSession);
+
   return (
     <>
-      <section className="welcome logged-in">Welcome username!</section>
+      <section className="welcome logged-in">{`Welcome ${session.displayName}!`}</section>
       <Posts />
     </>
   );
--- src/features/login/index.jsx
+++ src/features/login/index.jsx
@@ -1,23 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { sessionActions, fetchAuthTokenAsync } from "../../app/sessionSlice";
 import "./index.css";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchAuthTokenAsync({ username: username, password: password })
+      .then((data) => {
+        dispatch(
+          sessionActions.setSession({
+            username: username,
+            displayName: data.user_display_name,
+            email: data.user_email,
+            isAuthenticated: true,
+          })
+        );
+        history.push("/");
+      })
+      .catch((error) => {
+        setErrors(error.response || "Failed to login");
+      });
+  };
+
   return (
     <div>
       <h1>Login</h1>
+      <p className="error">{errors}</p>
 
       <div className="login">
         <form method="post">
           <div>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" name="username" />
+            <input
+              id="username"
+              type="text"
+              name="username"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
           </div>
           <div>
             <label htmlFor="password">Password</label>
-            <input id="password" type="password" name="password" />
+            <input
+              id="password"
+              type="password"
+              name="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
           </div>
           <div>
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Submit" onClick={handleSubmit} />
           </div>
         </form>
       </div>
